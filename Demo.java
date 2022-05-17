4bfd14c4fe499a917b771fea67c5e8a580367ac7
Rishon-A-Singh <rishon.singh@gmail.com>
2022-05-17 20:12:31 +0530

    test commit

---
+++ Demo.java
@@ -0,0 +1,14 @@
+package com.masai;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Name: Rishon");
+		System.out.println("Father’s name: Rinka");
+		System.out.println("Mother’s name: Vijaya");
+		System.out.println("Age: 25");
+		System.out.println("Gender: Male");
+		System.out.println("Mobile No: 9880444224");
+	}
+}
---
+++ PrimeFactor.java
@@ -0,0 +1,19 @@
+package com.masai;
+
+public class PrimeFactor {
+
+	public static void main(String[] args) {
+		int num = 548;
+		int arr = [];
+				
+			for(int i = 2;i<num; i++) {
+				
+				while(num%2==0) {
+					
+				}
+					
+			}
+
+	}
+
+}
