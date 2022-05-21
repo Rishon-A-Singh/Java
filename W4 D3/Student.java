cc3ea1e40b9980fc4c7ecaa38d11738a33ba9cea
Rishon-A-Singh <101565845+Rishon-A-Singh@users.noreply.github.com>
2022-05-21 17:22:29 +0530

    Add files via upload

---
+++ W4 D3/Student.java
@@ -0,0 +1,29 @@
+package com.masai;
+
+public class Student {
+	
+	int roll;
+	String name;
+	int marks;
+	
+	void displayStudentDetails() {
+		System.out.println("Roll is:" + this.roll);
+		System.out.println("Name is:" + this.marks);
+		System.out.println("Marks is:" + this.marks);
+	}
+
+	public static void main(String[] args) {
+		Student s1 = new Student();
+		s1.roll = 996;
+		s1.name = "Rishon";
+		s1.marks = 99;
+		Student s2 = new Student();
+		s2.roll = 1;
+		s2.name="Someone else";
+		s2.marks=100;
+		s2.displayStudentDetails();
+		s1.displayStudentDetails();
+		
+	}
+
+}
