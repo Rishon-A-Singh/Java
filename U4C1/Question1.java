3cf0ae30026621253c30c79b1d82bf90b1341fca
Rishon-A-Singh <101565845+Rishon-A-Singh@users.noreply.github.com>
2022-05-23 15:46:30 +0530

    Create Question1.java

---
+++ U4C1/Question1.java
@@ -0,0 +1,36 @@
+package U4C1;
+
+public class Question1 {
+	
+	/*Question 1: Strings are immutable because this helps to 
+	 * make the language more safe from information being affected 
+	 * from malicious things. 
+	 * String builder is different from String because it uses less memory,
+	 * and is faster
+	 */
+
+	String example;
+	
+	public static void main(String[] args) {
+		// example 1
+	}
+	
+	public String getExample() {
+		return example;
+		//example 2
+	}
+	
+	public void setExample(String a) {
+		this.example = a;
+		
+		//example 3
+	}
+	
+	private static void AnotherStringMethod() {
+		Question1 example4 = new Question1();
+		
+	}
+	
+	
+
+}
